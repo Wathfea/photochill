04e54fe923c30b06b1c08a13a8f8ae78ada31874
Renata Edit Szabo <szabo.renata@zengo.eu>
2016-09-30 15:28:27 +0200

    del

--- web/app/plugins/modula-best-grid-gallery/admin/upgrade.php
+++
@@ -1,4 +0,0 @@
-<p>Redirecting to modula.greentreelabs.net</p>
-<script>
-	location.href = "http://modula.greentreelabs.net/?utm_source=modulalite_inst&utm_medium=upgradepage&utm_campaign=Modula%20Lite#buy";
-</script>
